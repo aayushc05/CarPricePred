c258001c3d9391320ff273cd019af04dc374be60
aayushc05 <48418018+aayushc05@users.noreply.github.com>
2021-03-08 17:34:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -25,10 +25,17 @@ def predict():
     Fuel_LPG=0
     Fuel_Diesel=0
     Fuel_Electric=0
+    Company_Mahindra=0
+    Company_others=0
+    Company_Tata=0
     Seller_Type_Dealer=0
     if request.method == 'POST':
         Year = int(request.form['year'])
         Kms_Driven=int(request.form['kms_driven'])
+        Mileage_Kmpl=int(request.form['mileage_kmpl'])
+        Engine_CC=int(request.form['engine_cc'])
+        Max_Power_BHP=int(request.form['max_power_bhp'])
+        Seats=int(request.form['seats'])
         Owner=int(request.form['owner'])
         Fuel_Petrol=request.form['fuel_Petrol']
         if(Fuel_Petrol=='Petrol'):
@@ -56,6 +63,32 @@ def predict():
                 Fuel_Diesel=0
                 Fuel_LPG=0
                 Fuel_Electric=0
+        Company_Maruti=request.form['company_Maruti']
+        if(Company_Maruti=='Maruti'):
+                Company_Maruti=1
+                Company_Tata=0
+                Company_Mahindra=0
+                Company_others=0              
+        elif(Company_Maruti=='Tata'):
+                Company_Maruti=0
+                Company_Tata=1
+                Company_Mahindra=0
+                Company_others=0
+        elif(Company_Maruti=='Mahindra'):
+                Company_Maruti=0
+                Company_Tata=0
+                Company_Mahindra=1
+                Company_others=0
+        elif(Company_Maruti=='others'):
+                Company_Maruti=0
+                Company_Tata=0
+                Company_Mahindra=0
+                Company_others=1
+        else:
+                Company_Maruti=0
+                Company_Tata=0
+                Company_Mahindra=0
+                Company_others=0
         Year=2020-Year
         Seller_Type_Individual=request.form['seller_type_Individual']
         if(Seller_Type_Individual=='Individual'):
@@ -72,7 +105,7 @@ def predict():
             Transmission_Manual=1
         else:
             Transmission_Manual=0
-        prediction=model.predict([[Kms_Driven,Owner,Year,Fuel_Diesel,Fuel_Petrol,Fuel_Electric,Fuel_LPG,Seller_Type_Individual,Seller_Type_Dealer,Transmission_Manual]])
+        prediction=model.predict([[Kms_Driven,Owner,Mileage_Kmpl,Engine_CC,Max_Power_BHP,Seats,Year,Company_Mahindra,Company_Maruti,Company_Tata,Company_others,Fuel_Diesel,Fuel_Petrol,Fuel_Electric,Fuel_LPG,Seller_Type_Individual,Seller_Type_Dealer,Transmission_Manual]])
         output=round(prediction[0],0)
         if output<0:
             return render_template('Index.html',prediction_texts="Sorry you cannot sell this car")
